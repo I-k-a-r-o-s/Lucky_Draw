00174fde930ae9e12be813b7c30c25f1c31930d7
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2023-08-18 18:54:43 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Lucky_Draw
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rchtxtoutput = new System.Windows.Forms.RichTextBox();
+            this.txtinput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(15, 114);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Your Lucky Number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 183);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Your Numbers";
+            // 
+            // rchtxtoutput
+            // 
+            this.rchtxtoutput.Location = new System.Drawing.Point(176, 141);
+            this.rchtxtoutput.Name = "rchtxtoutput";
+            this.rchtxtoutput.Size = new System.Drawing.Size(100, 96);
+            this.rchtxtoutput.TabIndex = 3;
+            this.rchtxtoutput.Text = "";
+            // 
+            // txtinput
+            // 
+            this.txtinput.Location = new System.Drawing.Point(176, 46);
+            this.txtinput.Name = "txtinput";
+            this.txtinput.Size = new System.Drawing.Size(100, 20);
+            this.txtinput.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(302, 260);
+            this.Controls.Add(this.txtinput);
+            this.Controls.Add(this.rchtxtoutput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Lucky Draw";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox rchtxtoutput;
+        private System.Windows.Forms.TextBox txtinput;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lucky_Draw
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();           
+            if(txtinput.Text.Length == 0 ) 
+            {
+                MessageBox.Show("Input lucky number", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    int nmbr = rand.Next(0, 20);
+                    rchtxtoutput.Text += nmbr.ToString() + Environment.NewLine;
+                    if (int.Parse(txtinput.Text) == nmbr)
+                    {
+                        MessageBox.Show("Congratulations!You have won", "", MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk);
+                    }                  
+                }
+            }         
+        }
+    }
+}
